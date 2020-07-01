2999b14203ee48dfb4ef9390cbfcf7a8c7d76549
jdavis3333 <jdavis3333@gmail.com>
2020-07-01 11:32:25 -0700

    page2 js functionality

---
+++ userInput/userInput.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+//  var foodType = "";
+//  console.log(foodType);
+ var answersArr = [];
+
+    //page 2 functionality
+    $("#page2btn").on("click", function() {
+        foodCategory = $("#foodCategory").val();
+        console.log(foodCategory);
+        console.log(JSON.stringify(foodCategory));
+        // console.log(foodType);
+        //take us to page 3
+    })
+
+
+
+    //page 3 functionality
+
+
+
+
+
+
+})
+
+
+

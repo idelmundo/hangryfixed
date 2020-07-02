5f004173101e5859d7e087af838833475cc32b78
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-07-02 10:20:49 -0700

    fixed yelp api call

--- API script/yelp-api.js
+++ API script/yelp-api.js
@@ -20,13 +20,13 @@ $(document).ready(function () {
             console.log(latitude)
 
 
-            var radius = radiusMiles * 1609;
-            // var testLongitude = longitude
-            // var testLatitude = latitude
-            var foodCategory = "pho"
+            // var radius = radiusMiles * 1609;
+            var testLongitude = longitude
+            var testLatitude = latitude
+            var foodCategory = "gelato"
             //when !category is entered, an error is logged onto console with the value "ERR_NAME_NOT_RESOLVED"
-            var price = "$$"
-            var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?categories=" + foodCategory + "&radius=" + radius + "&price=" + price + "&latitude=" + testLatitude + "&longitude=" + testLongitude;
+            var price = "2"
+            var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?categories=" + foodCategory +  "&price=" + price + "&latitude=" + testLatitude + "&longitude=" + testLongitude;
 
             $.ajax({
                 url: myurl,
@@ -64,17 +64,15 @@ $(document).ready(function () {
 
     };
     $("#saveBtn").on("click", function (event) {
+        event.preventDefault();
         function buildUserAdrress() {
-            // var userAddressParams = {};
             var userAddressInput = $("#address-input").val().toLowerCase().split(",");
             console.log(userAddressInput);
-
             var streetAddress = (userAddressInput[0].toLowerCase().split(" "))
-
             return encodeURI(streetAddress);
-
         };
         fetchData(buildUserAdrress())
+        // window.location.assign("../userInput/page2.html");
 
     });
 
